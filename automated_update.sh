f208cc50ac8614e4693bb7946f97e55e06252027
System Administrator <root@Alexs-MacBook-Pro.local>
2020-12-18 16:38:19 -0800

    automated update

--- automated_update.sh
+++ automated_update.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
+date
+set -e
 set -x
 cd /Users/alexgurvets/projects/hackermd-github-pages/agurvets.github.io
 git pull
-python3 fetch_data.py
-python3 frozen_flask.py
+pip3 install requests Frozen-Flask humanize
+/usr/bin/python3 fetch_data.py
+/usr/bin/python3 frozen_flask.py
 cp -R hackermd/src/build/* .
 git add --all
 git commit -m "automated update"
